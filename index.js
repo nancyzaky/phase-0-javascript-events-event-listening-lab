c4a1a6910aaab93f2ae23c7a95fb6e977043eb5b
Nancy <nancyzaky29@gmail.com>
2021-05-23 23:34:04 -0700

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+   alert('I was clicked!');
+   const input = document.getElementById('input');
+   input.addEventListener('click', addingEventListener)
 }
+input.addEventListener('click', addingEventListener)
